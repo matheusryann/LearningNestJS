62cc79bb840d222cd039797f3a25bead009ff1da
matheusryann <matheus.ryan@discente.ufma.br>
2026-06-12 22:09:11 -0300

    feat: add user retrieval and update routes to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { Prisma, UserModel } from 'src/generated/prisma/client';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { Prisma, User } from 'src/generated/prisma/client';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService)
+    constructor(private readonly usersService: UsersService) {}
 
  @Post('signup')
- async signUp(@Body() userData: Prisma.UserCreateInput): Promise<UserModel> {
+ async signUp(@Body() userData: Prisma.UserCreateInput): Promise<User> {
     return this.usersService.createUser(userData);
 
  }
+
+ @Get(':id')
+ async getUser(@Param('id') id : string): Promise<User | null> {
+   return this.usersService.findUser({id: Number(id)});
+ }
+
+ @Put() 
+ async updatadeUser(@Body() userData: Prisma.UserUpdateInput, @Param('id') id: string): Promise<User> {
+   return this.usersService.updateUser({where: {id: Number(id)}, data: userData});
+}
+
+@Delete(':id')
+async deleteUser(@Param('id') id: string): Promise<User> {
+   return this.usersService.deleteUser({id: Number(id)});
+}
 
 }
